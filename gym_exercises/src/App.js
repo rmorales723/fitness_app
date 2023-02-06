42cbe2ee97bd81c34d9546615d6c22678fbb2224
robert morales <rmoralfit@gmail.com>
2023-02-06 16:44:30 -0500

    problems rendering Home component to browser

--- gym_exercises/src/App.js
+++ gym_exercises/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Route, Router, Switch } from 'react-router-dom';
+import './App.css';
+import { Box } from '@mui/material';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{width: { xl: '1488px' }}} m="auto">
+      <Navbar />
+      <Switch>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Switch> 
+      <Footer />
+    </Box>
   )
 }
 
--- gym_exercises/src/index.js
+++ gym_exercises/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+  document.getElementById('root')
+    
 )
 
