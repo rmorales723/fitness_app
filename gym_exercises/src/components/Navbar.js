7ddd324cdba8f9e8e143f4f8f5f3c0b152906720
robert morales <rmoralfit@gmail.com>
2023-02-12 10:03:24 -0500

    starting to style navbar

--- gym_exercises/src/components/Navbar.js
+++ gym_exercises/src/components/Navbar.js
@@ -10,10 +10,16 @@ const Navbar = () => {
    <Stack>
     <Link to="/">
         <img src={Nationals} alt="Nationals" style={{
-        width: '28px', height: '48px', margin: '0 10px'
+        width: '28px', height: '50px',
+        margin: '0 10px'
         }} />
     </Link>
-    <Stack>
+    <Stack
+        direction="row"
+        gap="40px"
+        fontSize="24px"
+        alignItems="flex-end"
+        >
         <Link to="/" style={{ textDecoration: 'none', color: "#3A1212",
         borderBottom: '3px solid #FF2625'}}>Home</Link>
         <a href="#exercises" style={
