7b2f30cb7638a9252de3254e956b3e7efbb59970
robert morales <rmoralfit@gmail.com>
2023-02-12 10:41:55 -0500

    created componenents in home page(no logic yet)

--- gym_exercises/src/components/Navbar.js
+++ gym_exercises/src/components/Navbar.js
@@ -10,10 +10,10 @@ const Navbar = () => {
    <Stack 
    direction="row"
         justifyContent="space-around" sx={{gap:{
-            sm: '122px', xs: '40px'}, mt: { sm: '32px', xs: '20px'}, justifyContent: 'none'}}>
+            sm: '122px', xs: '40px'}, mt: { sm: '32px', xs: '20px'}, justifyContent: 'none'}} px="20px">
     <Link to="/">
         <img src={Nationals} alt="Nationals" style={{
-        width: '28px', height: '50px',
+        width: '32px', height: '50px',
         margin: '0 10px'
         }} />
     </Link>
