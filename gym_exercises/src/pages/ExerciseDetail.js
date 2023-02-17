f2dccadfe8eba0130f4d3da07968cffaf0c17073
robert morales <rmoralfit@gmail.com>
2023-02-16 20:02:36 -0500

    detail.js

--- gym_exercises/src/pages/ExerciseDetail.js
+++ gym_exercises/src/pages/ExerciseDetail.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   return (
